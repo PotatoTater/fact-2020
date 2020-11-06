364283c1ca1d48c42bd30060df14fec218b16640
PotatoTater <51422468+PotatoTater@users.noreply.github.com>
2020-11-06 14:13:08 -0600

    trying zoom link in agenda

--- src/scenes/LandingPlaceholder/index.js
+++ src/scenes/LandingPlaceholder/index.js
@@ -132,7 +132,7 @@ const Agenda = () => (
         time: "3:00 - 7:00pm CST",
         duration: 1,
         title: "Virtual *Onsite* Registration",
-        location: "Zoom",
+        location: <a href="https://us02web.zoom.us/j/83529638108?pwd=SDh6MnNOQTlwcno1QVVRUExuSE9zQT09">Zoom</a>,
       }
     ]}/>
     </Col>
